3bec214492da4d8b20579dd9b4166ec2b93fb6c6
J <jpham26@gmail.com>
2023-02-20 16:08:20 -0800

    Add files via upload

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'django-insecure-%@g@m-n)hz=2cjsoulh(r#oeox!s74%^n%4%=0%0$4ctgk07n_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
